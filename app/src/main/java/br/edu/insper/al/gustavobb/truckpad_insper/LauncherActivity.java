848f313a48df8ab79a20be1cbc61997393a86f18
Gustavo <Gustavobb@al.insper.edu.br>
2019-05-08 17:41:04 -0300

    initial files

---
+++ app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/LauncherActivity.java
@@ -0,0 +1,13 @@
+package br.edu.insper.al.gustavobb.truckpad_insper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
