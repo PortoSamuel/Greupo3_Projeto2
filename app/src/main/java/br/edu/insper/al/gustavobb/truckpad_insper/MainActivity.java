7f00a6b0a82190bfee9c377bdd9951ed528cc2a0
Caio Fauza <caiofauza@gmail.com>
2019-05-12 23:57:56 -0300

    Layout Inicial

---
+++ app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/MainActivity.java
@@ -0,0 +1,92 @@
+package br.edu.insper.al.gustavobb.truckpad_insper;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView sideBarListView;
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private Spinner spinnerTruckLoad, spinnerAxleNumbers, spinnerReturnLoad;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sideBarListView = findViewById(R.id.sideBarList);
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer);
+        spinnerTruckLoad = findViewById(R.id.spinnerTruckLoad);
+        spinnerAxleNumbers = findViewById(R.id.spinnerAxleNumbers);
+        spinnerReturnLoad = findViewById(R.id.spinnerReturnLoad);
+
+
+        setupSideBar();
+        setSupportActionBar(toolbar);
+        setupSpinners();
+
+
+
+
+
+    }
+
+    private void setupSpinners(){
+        //Truckload
+        ArrayAdapter<CharSequence> truckLoadAdapter = ArrayAdapter.createFromResource(this, R.array.truckload, android.R.layout.simple_spinner_item);
+        truckLoadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTruckLoad.setAdapter(truckLoadAdapter);
+
+        //AxleNumbers
+        ArrayAdapter<CharSequence> axleNumbersAdapter = ArrayAdapter.createFromResource(this, R.array.axlesNumber, android.R.layout.simple_spinner_item);
+        axleNumbersAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAxleNumbers.setAdapter(axleNumbersAdapter);
+
+        //ReturnLoad
+        ArrayAdapter<CharSequence> returnLoadAdapter = ArrayAdapter.createFromResource(this, R.array.returnLoad, android.R.layout.simple_spinner_item);
+        returnLoadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerReturnLoad.setAdapter(returnLoadAdapter);
+
+
+    }
+
+
+    private void setupSideBar() {
+        String[] sideBarOptions = {"Home", "2"};
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sideBarOptions);
+        sideBarListView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.actionSideBar:
+                drawer.openDrawer(Gravity.LEFT);
+        }
+        return true;
+    }
+
+
+
+}
