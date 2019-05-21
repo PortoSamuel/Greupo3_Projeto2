080f4fbe675c58853fcdf2d5765a63273ba64531
PortoSamuel <samuelnnp@al.insper.edu.bbr>
2019-05-21 01:32:12 -0300

    POST calculando frete

--- app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/Price.java
+++ app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/Price.java
@@ -1,4 +1,12 @@
 package br.edu.insper.al.gustavobb.truckpad_insper;
 
 public class Price {
+
+    private double shipment_value;
+
+    public double getShipment_value(){
+        return shipment_value;
+    }
+
+
 }
--- app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/PriceInformation.java
+++ app/src/main/java/br/edu/insper/al/gustavobb/truckpad_insper/PriceInformation.java
@@ -1,4 +1,15 @@
 package br.edu.insper.al.gustavobb.truckpad_insper;
 
 public class PriceInformation {
+    private int axis;
+    private double distance;
+    private boolean has_return_shipment;
+    private String type;
+
+    public PriceInformation(int axis, double distance, boolean has_return_shipment, String type){
+        this.axis = axis;
+        this.distance = distance;
+        this.has_return_shipment = has_return_shipment;
+        this.type = type;
+    }
 }
